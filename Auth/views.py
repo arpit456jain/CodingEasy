e338233327b94dd19b8ad8712126d4e83fdb72c0
SAIKAT_CLAW <65228695+7saikat7@users.noreply.github.com>
2022-01-07 18:19:53 +0530

    Revert "Added 3 Apps"

--- Auth/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def auth(request):
-    return HttpResponse("<h2 align= 'center' > Hello Auth</h2>")
