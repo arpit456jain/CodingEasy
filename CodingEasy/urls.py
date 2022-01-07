5d2d3e2ef42782f039b30faa5ee0511ed8e5cb65
Atif0604 <atifcomputer2001@gmail.com>
2022-01-07 12:14:21 +0530

    Added 3 Apps

--- CodingEasy/urls.py
+++ CodingEasy/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
+    path('', include("Auth.urls")),
+    path('', include("DashBoard.urls")),
+    path('', include("Home.urls")),
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('login',views.login,name='login'),
