f49ebe66ce6013d54f7c3fd511ba9a22166ae6b5
Harikishan <harikishantk@gmail.com>
2022-01-28 17:50:32 +0530

    fix: uncomment the comments

--- DashBoard/admin.py
+++ DashBoard/admin.py
@@ -1,4 +1,9 @@
 from pyexpat import model
 from django.contrib import admin
+from .models import Author
 from import_export.admin import ImportExportModelAdmin
 # Register your models here.
+class AuthorAdmin(ImportExportModelAdmin):
+    pass
+
+admin.site.register(Author, AuthorAdmin) 
