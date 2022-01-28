8a6032016bd29e0db6cbb21b5a213a271122f088
Harikishan <harikishantk@gmail.com>
2022-01-28 17:09:39 +0530

    fix: removed comments

--- DashBoard/admin.py
+++ DashBoard/admin.py
@@ -1,12 +1,4 @@
 from pyexpat import model
 from django.contrib import admin
-# from .models import Author
 from import_export.admin import ImportExportModelAdmin
 # Register your models here.
-# If you want to download your model data as csv, you have to register your model
-# as like the example shown here.
-
-# class AuthorAdmin(ImportExportModelAdmin):
-#     pass
-
-# admin.site.register(Author, AuthorAdmin)
