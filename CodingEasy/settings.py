37438e21769379402cff3323c4c8a26843639916
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-02-05 17:15:08 +0530

    Refactoring

--- CodingEasy/settings.py
+++ CodingEasy/settings.py
@@ -139,4 +139,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dbmsprojekt@gmail.com'
-EMAIL_HOST_PASSWORD = 'dbms@project123'
+EMAIL_HOST_PASSWORD = 'XXXXXX'
