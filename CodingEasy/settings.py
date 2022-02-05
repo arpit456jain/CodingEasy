10332c9fb12faf39e7c5b3ea3d9991f27d116f98
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-02-05 17:21:16 +0530

    Refactoring

--- CodingEasy/settings.py
+++ CodingEasy/settings.py
@@ -131,10 +131,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
