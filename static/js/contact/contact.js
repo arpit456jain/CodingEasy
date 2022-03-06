91cffae4041d6e19b42efca4705b8832ef6751e0
Arshey Belkhode <11.21.22.arsh@gmail.com>
2022-03-06 12:19:03 +0530

    Initial Commit

--- static/js/contact/contact.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-  });
