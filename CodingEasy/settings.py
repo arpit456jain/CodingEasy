a75912288bf6293bc46ca33503d4d4208eaa37bf
Muhammad-Galhoum <muhammadgalhoum@gmail.com>
2022-03-12 14:57:31 +0200

    store the details from contact us page to database and give a msg of success or failure

--- CodingEasy/settings.py
+++ CodingEasy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name=models.CharField(max_length=50,null=False, blank=False)
+    email=models.EmailField(max_length=50,null=False, blank=False)
+    message=models.TextField(null=False, blank=False)
+    
+    def __str__(self):
+        return f'Message from {self.name}'
