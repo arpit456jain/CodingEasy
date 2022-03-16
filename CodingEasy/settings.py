a2b94e1eceb4d177534d1200020940c11a4a657c
Muhammad-Galhoum <muhammadgalhoum@gmail.com>
2022-03-16 23:07:11 +0200

    Edit the settings.py file

--- CodingEasy/settings.py
+++ CodingEasy/settings.py
@@ -127,10 +127,3 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'muhammadgalhoum@gmail.com' #os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = 'Google&Microsoft_86'#os.environ.get('EMAIL_PASS')
