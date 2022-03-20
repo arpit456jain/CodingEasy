4a5e2253c524b4367ff23c4ce7593bf66ee4551c
Nayan <ndas5144@gmail.com>
2022-03-21 00:02:20 +0530

    Added missing favicon

--- CodingEasy/urls.py
+++ CodingEasy/urls.py
@@ -17,7 +17,15 @@ from unicodedata import name
 from django.contrib import admin
 from django.urls import path, include
 
+#for favison
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
 
