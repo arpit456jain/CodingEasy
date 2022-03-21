6413a5a8810400983490bbe2d72f4e9532af2745
Arshey Belkhode <11.21.22.arsh@gmail.com>
2022-03-21 23:14:18 +0530

    Added Blog

--- CodingEasy/settings.py
+++ CodingEasy/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
     'blog.apps.BlogConfig',
     'crispy_forms',
 ]
