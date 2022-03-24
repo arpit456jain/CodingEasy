1a5a46254dc59cbc17a7f7e0d912deaf827ccc8d
Muhammad-Galhoum <muhammadgalhoum@gmail.com>
2022-03-24 13:40:56 +0200

    Solving the conflict of forms.py file

--- home/forms.py
+++ home/forms.py
@@ -5,11 +5,14 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model= Contact
         fields=['name','email','message']
-
-
+        
+        
+        
 # newsletter form
 
 class NewsletterForm(forms.ModelForm):
     class Meta:
         model = Newsletter
         fields = ['email']
+    
+    
