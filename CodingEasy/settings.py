98383a9dfc3fb50bc16691bf815cfc6a2230eb10
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2022-03-26 14:21:31 +0530

    bug: removed redundant app definition

--- CodingEasy/settings.py
+++ CodingEasy/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'home.apps.HomeConfig',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
