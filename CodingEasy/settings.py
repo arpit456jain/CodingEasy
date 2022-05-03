c1f80da782e4f90560dd37e974cf7c33e40fce04
“Arpit <111arpit1@gmail.com>
2022-05-03 12:17:17 +0530

    edit in settings for heroku

--- CodingEasy/settings.py
+++ CodingEasy/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home.apps.HomeConfig',
-    'blog.apps.BlogConfig',
+    'home',
+    'blog',
     'crispy_forms',
 ]
 
