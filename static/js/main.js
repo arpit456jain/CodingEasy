000dbffd6d05a1e6380a3dc662993afb1f552646
Jay <mkvjayanth1@gmail.com>
2022-05-30 01:19:23 +0530

    sidenav added

--- static/js/main.js
+++ static/js/main.js
@@ -17,11 +17,3 @@ behavior: "smooth",
 });
 
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-function closeNav() {
-document.getElementById("mySidenav").style.width = "0";
-}
-
